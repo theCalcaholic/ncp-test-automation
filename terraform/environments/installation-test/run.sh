38872a83cbd14a8151355b8f06927261c2900143
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2021-07-07 16:43:12 +0200

    run.sh: Execute all tests even if some of them failed

--- terraform/environments/installation-test/run.sh
+++ terraform/environments/installation-test/run.sh
@@ -7,7 +7,7 @@
 
   branch="${1:-devel}"
   tmpdir="$(mktemp -d)"
-  trap "terraform destroy; rm -rf \$tmpdir; ssh -S ncp-testing -O exit root@\${ipv4_address}" EXIT 1 2 3 4
+  trap "terraform destroy; rm -rf \$tmpdir; ssh -S ncp-testing -O exit root@\${ipv4_address}" EXIT 1 2
   terraform apply -var="branch=${branch}"
   ipv4_address="$(terraform output -raw test_server_ipv4)"
   ssh-keygen -f "$HOME/.ssh/known_hosts" -R "${ipv4_address}" 2> /dev/null
@@ -22,18 +22,25 @@
   cd nextcloudpi/tests
   git checkout "${branch}"
 
+  failed=no
+
   python activation_tests.py "localhost" "8443" "9443" || {
     echo "Activation test failed!"
-    exit 2
+
+    read -rp "Continue anyway? (y|N)" choice
+    [[ "${choice,,}" == "y" ]] || exit 2
+    failed=yes
   }
   python system_tests.py "root@${ipv4_address}" || {
     echo "System test failed!"
-    exit 3
+    failed=yes
   }
   python nextcloud_test.py "localhost" "8443" "9443" || {
     echo "Nextcloud test failed!"
-    exit 4
+    failed=yes
   }
+
+  [[ "failed" != "yes" ]] || exit 2
 
   echo "All tests succeeded"
 
