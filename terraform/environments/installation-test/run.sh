4e89de635e6a13416f70b3ba0629f5f2bdcbbe0f
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2021-07-07 17:15:51 +0200

    run.sh: Various fixes
    - Fix ssh socket cleanup
    - Fix terraform destroy not run during cleanup

--- terraform/environments/installation-test/run.sh
+++ terraform/environments/installation-test/run.sh
@@ -2,18 +2,23 @@
 
 (
   set -e
-  cd "$(dirname "$0")"
+  script_dir="$(realpath "$(dirname "$0")")"
+  cd "$script_dir"
   echo "Setting up infrastructure"
 
   branch="${1:-devel}"
   tmpdir="$(mktemp -d)"
-  trap "terraform destroy; rm -rf \$tmpdir; ssh -S ncp-testing -O exit root@\${ipv4_address}" EXIT 1 2
+  ssh_control_socket="/dev/shm/ncp-testing-$RANDOM"
+  # shellcheck disable=SC2064
+  trap "set -x; set +e; rm -rf \$tmpdir; ssh -S \"\$ssh_control_socket\" -O exit root@\${ipv4_address}; cd $script_dir && terraform destroy" EXIT 1 2
   terraform apply -var="branch=${branch}"
   ipv4_address="$(terraform output -raw test_server_ipv4)"
   ssh-keygen -f "$HOME/.ssh/known_hosts" -R "${ipv4_address}" 2> /dev/null
   ssh -o "StrictHostKeyChecking=no" \
-    -M -S ncp-testing -fNT \
-    -L 8443:127.0.0.1:443 -L 9443:127.0.0.1:4443 root@"${ipv4_address}"
+    -M -S "$ssh_control_socket" -fNT \
+    -L 8443:127.0.0.1:443 -L 9443:127.0.0.1:4443 root@"${ipv4_address}" \
+    sleep 600
+  echo "ssh pid: $SSH_PID"
   cd "$tmpdir"
   virtualenv "$tmpdir/venv"
   . "$tmpdir/venv/bin/activate"
@@ -40,7 +45,7 @@
     failed=yes
   }
 
-  [[ "failed" != "yes" ]] || exit 2
+  [[ "$failed" != "yes" ]] || exit 2
 
   echo "All tests succeeded"
 
