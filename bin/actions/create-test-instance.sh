d66f0a6b0b8a63019fc1f44c08c2f70d9372b224
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2022-04-18 06:55:45 +0200

    create-test-instance.sh: Fix invalid variable name

--- bin/actions/create-test-instance.sh
+++ bin/actions/create-test-instance.sh
@@ -20,7 +20,7 @@ ensure-postinstall-snapshot "${ssh_pubkey_fprint}" "${branch}"
 tf-apply "$TF_TEST_ENV" "$TF_VAR_FILE" \
 -var="snapshot_type=ncp-postinstall" \
 -var="branch=${branch}" \
--var="admin_ssh_pubkey_fingerprint=${{ steps.prepare-environment.outputs.ssh_pubkey_fprint }}"
+-var="admin_ssh_pubkey_fingerprint=${ssh_pubkey_fprint}"
 
 echo "::set-output name=server_address::$(tf-output "$TF_TEST_ENV" test_server_ipv4)"
 echo "::set-output name=snapshot_id::$(tf-output "$TF_TEST_ENV" snapshot_id)"
