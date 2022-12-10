286aea0cf3b19e95d7c8d7262398850611b5bc55
thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>
2022-12-10 19:01:15 +0100

    snapshot-provider/main.tf: Fix reactivation of root user after installation

--- terraform/tasks/ncp-postinstall/snapshot-provider/main.tf
+++ terraform/tasks/ncp-postinstall/snapshot-provider/main.tf
@@ -17,9 +17,8 @@ module snapshot-provider {
     "set -e",
     "export BRANCH=\"${var.branch}\"",
     "export DBG=x",
-    "bash -c 'bash <(wget -O - https://raw.githubusercontent.com/nextcloud/nextcloudpi/${var.branch}/install.sh)' | tee /var/log/ncp-install.log || echo \"SOMETHING WENT WRONG (exit code $?)\"",
     # Reenable root user for ssh access
-    "sed -i '/^root/s/\\/usr\\/sbin\\/nologin/\\/bin\\/bash/' /etc/passwd",
+    "bash -c 'bash <(wget -O - https://raw.githubusercontent.com/nextcloud/nextcloudpi/${var.branch}/install.sh)' | tee /var/log/ncp-install.log || echo \"SOMETHING WENT WRONG (exit code $?)\"; sed -i '/^root/s/\\/usr\\/sbin\\/nologin/\\/bin\\/bash/' /etc/passwd",
     # Avoids mariadb being killed (by poweroff)
     "systemctl stop mariadb",
     "systemctl poweroff"
