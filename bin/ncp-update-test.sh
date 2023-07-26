06469105448dc9109052d89a008fd2f4b924e9b8
thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>
2023-07-26 22:21:06 +0200

    ncp-update-test.sh: Fix variable names

--- bin/ncp-update-test.sh
+++ bin/ncp-update-test.sh
@@ -3,26 +3,21 @@
 from="${1:-master}"
 to="${2:-devel}"
 
-set -e
+set -ex
 
 . "$(cd "${BASHSOURCE[0]}"; pwd)/library.sh"
 
 echo "Initialize Terraform"
-for path in "$tf_project_setup" "$tf_test_env"
+for path in "$TF_PROJECT_SETUP" "$TF_TEST_ENV"
 do
-  echo "Initializing $(basename "$path")..."
-  (
-  cd "$path" || exit 1
-  terraform init
-  )
-  echo "done"
+  tf-init "$path";
 done
 
 echo "Setting up project"
 
 hcloud-clear-root-key
-tf-apply "$tf_project_setup" "$var_file"
-ssh_pubkey_fprint="$(tf-output "$tf_project_setup" admin_ssh_pubkey_fingerprint)"
+tf-apply "$TF_PROJECT_SETUP" "$TF_VAR_FILE"
+ssh_pubkey_fprint="$(tf-output "$TF_PROJECT_SETUP" admin_ssh_pubkey_fingerprint)"
 
 if ! hcloud image list -t snapshot -l "type=ncp-postactivation,branch=${from//\//-}" -o noheader -o columns=created | grep -v day | grep -v week | grep -v year | grep -qv month
 then
@@ -33,13 +28,13 @@ cleanup() {
   (
     set +e
     terminate-ssh-port-forwarding "${ipv4_address}"
-    tf-destroy "$tf_test_env" "$var_file" -var="snapshot_type=ncp-postactivation" -var="branch=${from}" -var="admin_ssh_pubkey_fingerprint=${ssh_pubkey_fprint}"
+    tf-destroy "$TF_TEST_ENV" "$TF_VAR_FILE" -var="snapshot_type=ncp-postactivation" -var="branch=${from}" -var="admin_ssh_pubkey_fingerprint=${ssh_pubkey_fprint}"
   )
   exit "$1"
 }
 
-tf-apply "$tf_test_env" "$var_file" -var="snapshot_type=ncp-postactivation" -var="branch=${from}" -var="admin_ssh_pubkey_fingerprint=${ssh_pubkey_fprint}"
-ipv4_address="$(tf-output "$tf_test_env" test_server_ipv4)"
+tf-apply "$TF_TEST_ENV" "$TF_VAR_FILE" -var="snapshot_type=ncp-postactivation" -var="branch=${from}" -var="admin_ssh_pubkey_fingerprint=${ssh_pubkey_fprint}"
+ipv4_address="$(tf-output "$TF_TEST_ENV" test_server_ipv4)"
 
 ssh-keygen -f "$HOME/.ssh/known_hosts" -R "${ipv4_address}" 2> /dev/null
 ssh -o "StrictHostKeyChecking=no" "root@${ipv4_address}" "ncp-update '$to'" || {
@@ -49,3 +44,5 @@ ssh -o "StrictHostKeyChecking=no" "root@${ipv4_address}" "ncp-update '$to'" || {
 
 setup-ssh-port-forwarding "${ipv4_address}"
 test-ncp-instance -b "${to}" "root@${ipv4_address}" "localhost" "8443" "9443"
+
+[[ "$test_result" == "success" ]]
