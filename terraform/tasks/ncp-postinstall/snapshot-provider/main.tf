1e46f29794c87e379268d9521a8b71e3665a0c02
thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>
2023-10-26 23:06:31 +0200

    snapshot-provider/main.tf: Force bash shell for post_setup_script

--- terraform/tasks/ncp-postinstall/snapshot-provider/main.tf
+++ terraform/tasks/ncp-postinstall/snapshot-provider/main.tf
@@ -14,12 +14,13 @@ module snapshot-provider {
 
   admin_ssh_privkey = file(var.admin_ssh_privkey_path)
   post_setup_script = [
+    "#!/bin/bash",
     "set -e",
     "export BRANCH=\"${var.branch}\"",
     "export DBG=x",
     "trap 'systemctl stop mariadb; systemctl poweroff' EXIT",
     # Reenable root user for ssh access
-    "bash -c 'bash <(wget -O - https://raw.githubusercontent.com/nextcloud/nextcloudpi/${var.branch}/install.sh)' | tee /var/log/ncp-install.log; rc=$?; [[ \"rc\" == 0 ]] || echo \"SOMETHING WENT WRONG (exit code $?)\"; sed -i '/^root/s/\\/usr\\/sbin\\/nologin/\\/bin\\/bash/' /etc/passwd; exit $rc",
+    "bash -c 'bash <(wget -O - https://raw.githubusercontent.com/nextcloud/nextcloudpi/${var.branch}/install.sh)' | tee /var/log/ncp-install.log; rc=$?; [[ \"$rc\" == 0 ]] || echo \"SOMETHING WENT WRONG (exit code $?)\"; sed -i '/^root/s/\\/usr\\/sbin\\/nologin/\\/bin\\/bash/' /etc/passwd; exit $rc",
   ]
 
 }
